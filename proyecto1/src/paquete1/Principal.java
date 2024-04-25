c33ec3e31cb3a0f88f0361d3a66c9beab52c9b54
x <ad2738992@gmail.com>
2024-04-25 17:38:35 +0200

    "Segundo Commit"

--- proyecto1/src/paquete1/Principal.java
+++ proyecto1/src/paquete1/Principal.java
@@ -4,6 +4,7 @@ class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Estamos haiciendo control de versiones con Git");
+		System.out.println("Este es mi segundo commit");
 
 	}
 
